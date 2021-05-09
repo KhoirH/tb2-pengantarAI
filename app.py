c27fd76e2b14951b8b401b69e8f41bf39768fd4a
KhoirH <hilmi.khoirulloh02@gmail.com>
2021-05-09 12:31:27 +0700

    implement face recognition

--- app.py
+++ app.py
@@ -2,11 +2,18 @@
 # Imports
 #----------------------------------------------------------------------------#
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Response
 # from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 import os
+from imutils.video import VideoStream
+from imutils.video import FPS
+import face_recognition
+import numpy as np
+import imutils
+import cv2
+import time
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -14,25 +21,106 @@ import os
 
 app = Flask(__name__)
 app.config.from_object('config')
-
 #----------------------------------------------------------------------------#
 # Controllers.
 #----------------------------------------------------------------------------#
 
 
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('videostream.html')
 
-if not app.debug:
-    file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info('errors')
+@app.route('/video')
+def video():
+    return Response(generateFrames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def generateFrames():
+    videostream = VideoStream(src=0).start()
+    time.sleep(2.0)
+    
+    # Load a sample picture and learn how to recognize it.
+    obama_image = face_recognition.load_image_file("obama.jpg")
+    obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+
+    # Load a second sample picture and learn how to recognize it.
+    biden_image = face_recognition.load_image_file("biden.jpg")
+    biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+
+    # Create arrays of known face encodings and their names
+    known_face_encodings = [
+        obama_face_encoding,
+        biden_face_encoding
+    ]
+    known_face_names = [
+        "Barack Obama",
+        "Joe Biden"
+    ]
+
+    # Initialize some variables
+    face_locations = []
+    face_encodings = []
+    face_names = []
+    process_this_frame = True
+    while True:
+        frame = videostream.read()
+        frame = imutils.resize(frame, width=600)
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
+
+        # Only process every other frame of video to save time
+        if process_this_frame:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
+
+                # # If a match was found in known_face_encodings, just use the first one.
+                # if True in matches:
+                #     first_match_index = matches.index(True)
+                #     name = known_face_names[first_match_index]
+
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
+
+                face_names.append(name)
+
+        process_this_frame = not process_this_frame
+
+
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+
+            # Draw a box around the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+
+            # Draw a label with a name below the face
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+        cv2.imwrite('t.jpg', frame)
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+
+
 
 #----------------------------------------------------------------------------#
 # Launch.
--- config.py
+++ config.py
@@ -4,3 +4,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
+
+UPLOAD_FOLDER = '/Users/prate/upload/'
+ALLOWED_EXTENSIONS = set(['txt','mp4'])
