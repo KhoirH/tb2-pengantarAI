d8d797f137d50c2a581bad81d9ab036abf507f01
muhnayr <muhnayr@gmail.com>
2021-07-06 23:11:41 +0700

    category

--- app/main/controller/category_controller.py
+++ app/main/controller/category_controller.py
@@ -3,6 +3,7 @@ from flask_restx import Resource
 
 from app.main.util.decorator import admin_token_required
 from ..util.dto import AdminDto
+from app.main.service.category_service import get_all_category
 # from ..service.user_service import save_new_user, get_all_users, get_a_user
 from typing import Dict, Tuple
 
@@ -16,7 +17,8 @@ class CategoryView(Resource):
     # @api.marshal_list_with(_user, envelope='data')
     def get(self):
         headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('admin/category.html'),200,headers)
+        data = get_all_category()
+        return make_response(render_template('admin/category.html', categories = data),200,headers)
 
 
 
